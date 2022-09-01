0406716b4293a171ba23e11969580676d9ad1ff9
Rohan <awalrohan8@gmail.com>
2022-09-01 17:32:39 +0545

    brb

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-let home_scoreEL = document.getElementsByClassName("score");
+let home_scoreEL = document.getElementsByClassName("home_score")
 
 console.log(home_scoreEL)
 
 let count = 0
 
-function increment() {
+function increment_1() {
     count += 1
     home_scoreEL.textContent = count
     console.log(count)
 
+}
+
+function increment_2() {
+    count += 2
+    home_scoreEL.textContent = count
+    console.log(count)
+}
+
+function increment_3() {
+    count += 3
+
+    console.log(count)
 }
