16e31a30f6643acaf7acbc89069e40c7aa9eec90
Rohan <awalrohan8@gmail.com>
2022-09-01 14:31:47 +0545

    new

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello Woeld")
+let home_scoreEL = document.getElementsByClassName("score");
+
+console.log(home_scoreEL)
+
+let count = 0
+
+function increment() {
+    count += 1
+    home_scoreEL.textContent = count
+    console.log(count)
+
+}
